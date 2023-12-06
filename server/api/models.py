fa7d18b2d1f571beabaad0ec0e7b8f6166ff613c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-06 12:27:44 +0530

    add models

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fiction_score = models.FloatField()
+    non_fiction_score = models.FloatField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    fiction_score = models.FloatField()
+    non_fiction_score = models.FloatField()
