6bcaa38699b2aef96fa8c3eec10e562805c705d1
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-06 12:44:07 +0530

    add registertation form

--- server/core/views.py
+++ server/core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth import login
+from rest_framework.decorators import api_view
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def register_user(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect('/')
+    else:
+        form = CustomUserCreationForm()
+        print(form.errors)
+
+    return render(request, 'core/register.html', {'form': form})
